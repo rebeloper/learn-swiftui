ba73a72432823e5e18e5f860ece8ebe73a58d3d1
Alex Nagy <rebeloperalpar@gmail.com>
2023-12-01 10:37:46 +0200

    Initial Commit

---
+++ learn-swiftui/learn_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftuiApp.swift
+//  learn-swiftui
+//
+//  Created by Alex Nagy on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
